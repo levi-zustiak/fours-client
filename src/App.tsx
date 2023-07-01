1c503a3e98ad84ba1321f64f09a8920b31ae175b
Levi Zustiak <levizustiak@gmail.com>
2023-07-01 00:44:01 -0500

    [feat] create Game page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from '@solidjs/router';
 import { Create } from '@routes/Create';
 import { Join } from '@routes/Join';
 import { Home } from '@routes/Home';
+import { Game } from '@routes/Game';
 import { LobbyProvider } from '@contexts/LobbyContext';
 
 export function App() {
@@ -13,6 +14,7 @@ export function App() {
           <Route path="/" component={Home} />
           <Route path="/create" component={Create} />
           <Route path="/join/:id" component={Join} />
+          <Route path="/game/:id" component={Game} />
         </Routes>
       </LobbyProvider>
     </div>
---
+++ src/routes/Game.tsx
@@ -0,0 +1,7 @@
+import { Board } from '@components/game/Board';
+import { GameContextProvider } from '@contexts/GameContext';
+import { useLocation } from '@solidjs/router';
+
+export function Game() {
+  const { state } = useLocation();
+}
