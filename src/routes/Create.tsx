d3c4cb3133ae58dfb0ae8f869c04253fae039b26
Levi Zustiak <levizustiak@gmail.com>
2023-07-01 00:40:29 -0500

    [feat] update Create page to create a lobby

--- src/routes/Create.tsx
+++ src/routes/Create.tsx
@@ -1,3 +1,19 @@
+import { useLobby } from '@contexts/LobbyContext';
+import { createEffect, Show } from 'solid-js';
+
 export function Create() {
-  return <h1>Create</h1>;
+  const { state, create } = useLobby();
+
+  createEffect(() => create());
+  console.log(state);
+
+  return (
+    <div>
+      <h1>Create</h1>
+      <Show when={state.id} fallback={<h4>Loading</h4>}>
+        <h4>Lobby id: {state.id}</h4>
+      </Show>
+      <button>Create</button>
+    </div>
+  );
 }
