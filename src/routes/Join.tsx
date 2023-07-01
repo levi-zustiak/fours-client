7051e44183cda8aadc8a65f2f6c86223d83ff8ad
Levi Zustiak <levizustiak@gmail.com>
2023-07-01 00:40:41 -0500

    [feat] update Join page to join a lobby

--- src/routes/Join.tsx
+++ src/routes/Join.tsx
@@ -1,3 +1,12 @@
+import { useLobby } from '@contexts/LobbyContext';
+import { useNavigate, useParams } from '@solidjs/router';
+import { createEffect } from 'solid-js';
+
 export function Join() {
+  const params = useParams();
+  const { join } = useLobby();
+
+  createEffect(() => join(params.id));
+
   return <h1>Join</h1>;
 }
