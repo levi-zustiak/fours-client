d22d289bf930448ff65860fa8d2cf1976e13ff79
Levi Zustiak <levizustiak@gmail.com>
2023-08-29 22:44:00 -0500

    [wip] start refactor of pages in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
-import styles from './App.module.css';
-import { Route, Routes } from '@solidjs/router';
+import { A, Outlet, Route, Routes, useNavigate } from '@solidjs/router';
 import { Create } from '@routes/Create';
 import { Join } from '@routes/Join';
 import { Home } from '@routes/Home';
 import { Game } from '@routes/Game';
-import { LobbyProvider } from '@contexts/LobbyContext';
+import { Login } from '@routes/Login';
+import { Play } from '@routes/Play';
+import { Layout } from '@components/Layout';
+import { onMount } from 'solid-js';
+
+const RouteGuard = (props) => {
+  const navigate = useNavigate();
+
+  onMount(() => {
+    if (!props.authorized) {
+      navigate('/login', { replace: true });
+    }
+  });
+
+  return <Outlet />;
+};
 
 export function App() {
   return (
-    <div class={styles.App}>
-      <LobbyProvider>
-        <Routes>
-          <Route path="/" component={Home} />
+    <Routes>
+      <Route path="" component={Layout}>
+        <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="" element={<RouteGuard authorized={false} />}>
+          <Route path="/play" component={Play} />
           <Route path="/create" component={Create} />
           <Route path="/join/:id" component={Join} />
-          <Route path="/game/:id" component={Game} />
-        </Routes>
-      </LobbyProvider>
-    </div>
+        </Route>
+        <Route path="/game/:id" component={Game} />
+      </Route>
+      <Route path="*" element={() => <div>Page not found</div>} />
+    </Routes>
   );
 }
